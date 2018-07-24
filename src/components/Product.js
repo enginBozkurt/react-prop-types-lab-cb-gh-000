bca502f4b641baddf0bb00c43ab49246086ef161
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-24 12:51:50 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,17 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class  Product extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+        <p>{this.props.producer}</p>
+        <p>{this.props.hasWatermark}</p>
+        <p>{this.props.color}</p>
+      </div>
+    )
+  }
+}
+

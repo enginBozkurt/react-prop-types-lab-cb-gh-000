8b5dd6eb3804fc878141cdef0ccee9a18a42ed2b
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-24 13:05:05 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -23,7 +23,9 @@ Product.PropTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
   
+
 
 
 
